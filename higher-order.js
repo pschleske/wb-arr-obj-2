ee039b80a46c9f7243242dffe895dea4086fe801
Priscila Schleske <plasch7@gmail.com>
2023-08-07 22:08:55 -0600

    not done yetgit add .

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,7 @@ const mixedNumbers = [6, 3, 1, 7, 5, 2, 6, 8, 9, 4, 2, 7, 9, 3, 1, 8, 4, 3];
 */
 
 //Code Here
-let evenNumbers; // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(num => num % 2 === 0)
 
 ////////// PROBLEM 2 //////////
 
@@ -42,7 +42,7 @@ const prices = [15.0, 23.0, 78.0, 34.0, 12.0, 86.0, 12.0, 79.0, 32.0];
 */
 
 //Code Here
-let postTaxPrices; // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(num => num * 1.07);
 
 ////////// PROBLEM 3 //////////
 
@@ -59,7 +59,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation; //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(acc, el, index) => {
+  return acc + el
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -101,7 +103,7 @@ const monstersInYourPocket = [
 */
 
 //Code Here
-let myStrongest; // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+// let myStrongest = monstersInYourPocket.filter()
 
 ////////// PROBLEM 5 //////////
 
@@ -124,7 +126,10 @@ const orders = [
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals; // Code here
+let ordersTotal = orders.reduce((acc, el, index) => {
+  // console.log(el.price * el.tax + el.price)
+  return acc * el + el
+}, 0)
 
 ////////// PROBLEM 6 //////////
 
@@ -157,4 +162,4 @@ const purchases = [
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal; //Code Here
+// let bobsTotal = purchases = ((acc, el, index) => { }, 0)
